a56db07754cd1fa442f29b26cacb9ed268802211
Aaron Lee <aaronlab.net@gmail.com>
2020-11-30 21:18:25 +0900

    Add Service

--- FinancialApp/Services/AccountService.swift
+++ FinancialApp/Services/AccountService.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case baseURL
+    case decodingError
+    case noData
+}
+
+final class AccountService {
+    
+    private init() { }
+    
+    static let shared = AccountService()
+    
+    func getAllAccounts(completion: @escaping (Result<[Account]?, NetworkError>) -> Void) {
+        
+        guard let url = URL.urlForAccounts() else {
+            return completion(.failure(.baseURL))
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, error == nil else {
+                return completion(.failure(.noData))
+            }
+            
+            let accounts = try? JSONDecoder().decode([Account].self, from: data)
+            
+            if accounts == nil {
+                completion(.failure(.decodingError))
+            } else {
+                completion(.success(accounts))
+            }
+            
+        }.resume()
+        
+    }
+    
+}
