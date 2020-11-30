5510268634302c81a98dba23dfa34c11fd4a254f
Aaron Lee <aaronlab.net@gmail.com>
2020-11-30 21:54:17 +0900

    Allow http

---
+++ FinancialApp/Views/AccountListView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountListView.swift
+//  FinancialApp
+//
+//  Created by Aaron Lee on 2020-11-30.
+//
+
+import SwiftUI
+
+struct AccountListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountListView()
+    }
+}
