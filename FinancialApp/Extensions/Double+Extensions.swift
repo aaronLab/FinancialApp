e29fc576327c810c380ef8f7ebc4f61504494a37
Aaron Lee <aaronlab.net@gmail.com>
2020-11-30 21:54:33 +0900

    Change Locale

--- FinancialApp/Extensions/Double+Extensions.swift
+++ FinancialApp/Extensions/Double+Extensions.swift
@@ -11,6 +11,7 @@ extension Double {
     func formatAsCurrency() -> String {
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
+        formatter.locale = Locale(identifier: "es_CL")
         let formattedCurrency = formatter.string(from: self as NSNumber)
         return formattedCurrency ?? ""
     }
